41fafb22877a3e4defab22af3ee8ab29e961f923
심근웅 <geunwoong.sim@gmail.com>
2025-04-29 11:39:32 +0900

    Init AI_Calendar Project

---
+++ AI_Calendar/AI_Calendar/AI_CalendarApp.swift
@@ -0,0 +1,20 @@
+//
+//  AI_CalendarApp.swift
+//  AI_Calendar
+//
+//  Created by 심근웅 on 4/29/25.
+//
+
+import SwiftUI
+
+@main
+struct AI_CalendarApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
